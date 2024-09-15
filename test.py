d42c191cef2594e169896254768b728b9d48318f
Subhanshusethi <134162561+Subhanshusethi@users.noreply.github.com>
2024-09-16 04:45:27 +0800

    Update test.py
    chnaged image handling capacity

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-"""  
+"""
 Copyright (c) 2019-present NAVER Corp.
 MIT License
 """
@@ -28,6 +28,7 @@ import zipfile
 from craft import CRAFT
 
 from collections import OrderedDict
+
 def copyStateDict(state_dict):
     if list(state_dict.keys())[0].startswith("module"):
         start_idx = 1
@@ -58,10 +59,7 @@ parser.add_argument('--refiner_model', default='weights/craft_refiner_CTW1500.pt
 
 args = parser.parse_args()
 
-
-""" For test images in a folder """
-image_list, _, _ = file_utils.get_files(args.test_folder)
-
+# Create directories if they do not exist
 result_folder = './result/'
 picklefiles = './pickle'
 if not os.path.isdir(picklefiles):
@@ -73,26 +71,26 @@ if not os.path.isdir(result_folder):
 def test_net(net, image, text_threshold, link_threshold, low_text, cuda, poly, refine_net=None):
     t0 = time.time()
 
-    # resize
+    # Resize
     img_resized, target_ratio, size_heatmap = imgproc.resize_aspect_ratio(image, args.canvas_size, interpolation=cv2.INTER_LINEAR, mag_ratio=args.mag_ratio)
     ratio_h = ratio_w = 1 / target_ratio
 
-    # preprocessing
+    # Preprocessing
     x = imgproc.normalizeMeanVariance(img_resized)
     x = torch.from_numpy(x).permute(2, 0, 1)    # [h, w, c] to [c, h, w]
     x = Variable(x.unsqueeze(0))                # [c, h, w] to [b, c, h, w]
     if cuda:
         x = x.cuda()
 
-    # forward pass
+    # Forward pass
     with torch.no_grad():
         y, feature = net(x)
 
-    # make score and link map
+    # Make score and link map
     score_text = y[0,:,:,0].cpu().data.numpy()
     score_link = y[0,:,:,1].cpu().data.numpy()
 
-    # refine link
+    # Refine link
     if refine_net is not None:
         with torch.no_grad():
             y_refiner = refine_net(y, feature)
@@ -104,29 +102,29 @@ def test_net(net, image, text_threshold, link_threshold, low_text, cuda, poly, r
     # Post-processing
     boxes, polys = craft_utils.getDetBoxes(score_text, score_link, text_threshold, link_threshold, low_text, poly)
 
-    # coordinate adjustment
+    # Coordinate adjustment
     boxes = craft_utils.adjustResultCoordinates(boxes, ratio_w, ratio_h)
     polys = craft_utils.adjustResultCoordinates(polys, ratio_w, ratio_h)
         
     for k in range(len(polys)):
-        if polys[k] is None: polys[k] = boxes[k]
+        if polys[k] is None:
+            polys[k] = boxes[k]
 
     t1 = time.time() - t1
 
-    # render results (optional)
+    # Render results (optional)
     render_img = score_text.copy()
     render_img = np.hstack((render_img, score_link))
     ret_score_text = imgproc.cvt2HeatmapImg(render_img)
 
-    if args.show_time : print("\ninfer/postproc time : {:.3f}/{:.3f}".format(t0, t1))
+    if args.show_time:
+        print("\ninfer/postproc time : {:.3f}/{:.3f}".format(t0, t1))
 
     return boxes, polys, ret_score_text
 
-
-
 if __name__ == '__main__':
-    # load net
-    net = CRAFT()     # initialize
+    # Load net
+    net = CRAFT()  # Initialize
 
     print('Loading weights from checkpoint (' + args.trained_model + ')')
     if args.cuda:
@@ -141,7 +139,7 @@ if __name__ == '__main__':
 
     net.eval()
 
-    # LinkRefiner
+    # Link Refiner
     refine_net = None
     if args.refine:
         from refinenet import RefineNet
@@ -159,27 +157,32 @@ if __name__ == '__main__':
 
     t = time.time()
 
-    # load data
+    # Load data
     box_data = []
+    image_list, _, _ = file_utils.get_files(args.test_folder)
+    
     for k, image_path in enumerate(image_list):
         print("Test image {:d}/{:d}: {:s}".format(k+1, len(image_list), image_path), end='\r')
-        # print(f"Bounding boxes for {image_path}: {bboxes}")
-        image = imgproc.loadImage(image_path)
+        
+        try:
+            image = imgproc.loadImage(image_path)
+        except (OSError, ValueError) as e:
+            print(f"Error loading image {image_path}: {e}")
+            continue
 
         bboxes, polys, score_text = test_net(net, image, args.text_threshold, args.link_threshold, args.low_text, args.cuda, args.poly, refine_net)
-        # print(f"yeh hai pathhhh{bboxes}{image_path}")
-        # save score text
-        # box_len.append(bboxes)
+        
         file_name = os.path.basename(image_path)
         box_data.append({
             'file_name': file_name,
             'bboxes': bboxes
         })
-        filename, file_ext = os.path.splitext(os.path.basename(image_path))
-        # mask_file = result_folder + "/res_" + filename + '_mask.jpg'
-        # cv2.imwrite(mask_file, score_text)
-        with open(picklefiles + '/bbox_list.pkl', 'wb') as f:
-            pickle.dump(box_data, f)
-        
+
+        # Save results
         file_utils.saveResult(image_path, image[:,:,::-1], polys, dirname=result_folder)
-    print("elapsed time : {}s".format(time.time() - t))
+
+    # Save bounding boxes to pickle file after processing all images
+    with open(picklefiles + '/bbox_list.pkl', 'wb') as f:
+        pickle.dump(box_data, f)
+
+    print("Elapsed time : {}s".format(time.time() - t))
